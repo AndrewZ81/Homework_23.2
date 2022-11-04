60f4b96651cd35113a14158c82596e4efb6ade65
Andrew Zaiko <andrewzaiko@gmail.com>
2022-11-04 20:58:41 +0300

    Финальная версия

--- app/schemes.py
+++ app/schemes.py
@@ -7,3 +7,4 @@ class RequestSchema(Schema):
     value_1 = fields.Str(required=True)
     cmd_2 = fields.Str(required=True)
     value_2 = fields.Str(required=True)
+    file_name = fields.Str(required=True)
--- app/views/perform_query.py
+++ app/views/perform_query.py
@@ -1,7 +1,11 @@
-from flask import jsonify
-from flask import request
+from flask import request, jsonify
 from flask_restx import Resource, Namespace
+from marshmallow import ValidationError
 
+from app.schemes import RequestSchema
+from app.constructor import create_query
+
+request_schema = RequestSchema()
 queries_ns = Namespace("perform_query")
 
 
@@ -9,6 +13,12 @@ queries_ns = Namespace("perform_query")
 class QueriesView(Resource):
     def post(self):
         post_data = request.json
-        response = jsonify(post_data)
-        response.status_code = 201
-        return response
+
+        try:
+            request_schema.load(post_data)
+        except ValidationError as error:
+            return error.messages, 400
+
+        cmd_1, cmd_2 = post_data["cmd_1"], post_data["cmd_2"]
+        value_1, value_2 = post_data["value_1"], post_data["value_2"]
+        return jsonify(create_query(cmd_1, value_1, cmd_2, value_2))
